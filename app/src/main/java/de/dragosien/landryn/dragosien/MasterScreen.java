f6d2ce399a9e6c78403022626c599a621606b8fc
Jan Dreske <jan.dreske@mycelium.com>
2014-10-11 16:12:23 +0200

    formatting

--- app/src/main/java/de/dragosien/landryn/dragosien/MasterScreen.java
+++ app/src/main/java/de/dragosien/landryn/dragosien/MasterScreen.java
@@ -11,44 +11,39 @@ import de.dragosien.landryn.dragosien.fragments.DragballTableFragment;
 import de.dragosien.landryn.dragosien.fragments.LiveTickerFragment;
 import de.dragosien.landryn.dragosien.fragments.SearchPartnerFragment;
 
-
 public class MasterScreen extends ActionBarActivity {
 
-   private ViewPager masterViewPager;
-   private TabsAdapter tabsAdapter;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      ViewPager masterViewPager = new ViewPager(this);
+      masterViewPager.setId(R.id.pager);
+      setContentView(masterViewPager);
+      ActionBar bar = getSupportActionBar();
+      bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+      bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE);
+      TabsAdapter tabsAdapter = new TabsAdapter(this, masterViewPager);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-       masterViewPager = new ViewPager(this);
-       masterViewPager.setId(R.id.pager);
-       setContentView(masterViewPager);
-       ActionBar bar = getSupportActionBar();
-       bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-       bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE);
-       tabsAdapter = new TabsAdapter(this, masterViewPager);
+      //add all the nice tabs
+      tabsAdapter.addTab(bar.newTab().setText(getString(R.string.tab_partners)), SearchPartnerFragment.class, null);
+      tabsAdapter.addTab(bar.newTab().setText(getString(R.string.tab_tables)), DragballTableFragment.class, null);
+      tabsAdapter.addTab(bar.newTab().setText(getString(R.string.tab_ticker)), LiveTickerFragment.class, null);
+   }
 
-       //add all the nice tabs
-       tabsAdapter.addTab(bar.newTab().setText(getString(R.string.tab_partners)), SearchPartnerFragment.class, null);
-       tabsAdapter.addTab(bar.newTab().setText(getString(R.string.tab_tables)), DragballTableFragment.class, null);
-       tabsAdapter.addTab(bar.newTab().setText(getString(R.string.tab_ticker)), LiveTickerFragment.class, null);
-    }
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.master_screen, menu);
+      return true;
+   }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.master_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // handling of actionbar item clicks
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      // handling of actionbar item clicks
+      int id = item.getItemId();
+      if (id == R.id.action_settings) {
+         return true;
+      }
+      return super.onOptionsItemSelected(item);
+   }
 }
